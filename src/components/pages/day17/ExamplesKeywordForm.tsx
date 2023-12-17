cdd6a62e359aca59937c96f16c340d9fc6d2920f
thinkAmi <dev.thinkami+git@gmail.com>
2023-12-17 17:17:55 +0900

    add day17 examples

---
+++ src/components/pages/day17/ExamplesKeywordForm.tsx
@@ -0,0 +1,32 @@
+import {RJSFSchema} from "@rjsf/utils";
+import Form from "@rjsf/mui";  // MUI version
+import validator from "@rjsf/validator-ajv8";
+
+export const ExamplesKeywordForm = () => {
+  const schema: RJSFSchema = {
+    title: "Examples Keyword Form",
+    type: "object",
+    properties: {
+      stringInput: {
+        type: "string",
+        examples: ["foo", "bar"]
+      }
+    }
+  }
+
+  // onSubmitの引数の説明は以下
+  // https://rjsf-team.github.io/react-jsonschema-form/docs/api-reference/form-props#onsubmit
+  const onSubmit = ({formData}, _event) => {
+    console.log(formData)
+  }
+
+  return (
+    <div style={{width: '400px'}}>
+      <Form
+        schema={schema}
+        validator={validator}
+        onSubmit={onSubmit}
+      />
+    </div>
+  )
+}
